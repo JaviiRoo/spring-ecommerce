10b14a71cfc554ef8c9515860287019cb8da56be
Javier <javier_alcala07@hotmail.com>
2025-10-22 00:11:39 +0100

    Funcion para eliminar los productos

--- src/main/java/com/rodsan/ecommerce/controller/ProductoController.java
+++ src/main/java/com/rodsan/ecommerce/controller/ProductoController.java
@@ -65,5 +65,12 @@ public class ProductoController {
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable Integer id) {
+		
+		productoService.delete(id);
+		return "redirect:/productos";
+	}
 
 }
