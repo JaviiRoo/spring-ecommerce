08a1048d2bebaec0728bca41c28f191db47982a4
Javier <javier_alcala07@hotmail.com>
2025-10-25 15:47:06 +0100

    Método para generar el número de la orden

--- src/main/java/com/rodsan/ecommerce/services/IOrdenService.java
+++ src/main/java/com/rodsan/ecommerce/services/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.rodsan.ecommerce.services;
 
+import java.util.List;
+
 import com.rodsan.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	
+	List <Orden> findAll();
 	
 	Orden save (Orden orden);
 
