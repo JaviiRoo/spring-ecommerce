10113ab4c0f59dbcfa968a67e28423a2756c9386
Javier <javier_alcala07@hotmail.com>
2025-10-25 16:37:36 +0100

    Función para el registro de usuarios

--- src/main/java/com/rodsan/ecommerce/services/UsuarioServiceImpl.java
+++ src/main/java/com/rodsan/ecommerce/services/UsuarioServiceImpl.java
@@ -16,8 +16,14 @@ public class UsuarioServiceImpl implements IUsuarioService {
 
 	@Override
 	public Optional<Usuario> findById(Integer id) {
-		// TODO Auto-generated method stub
+
 		return usuarioRepository.findById(id);
+	}
+
+	@Override
+	public Usuario save(Usuario usuario) {
+		
+		return usuarioRepository.save(usuario);
 	} 
 	
 	
